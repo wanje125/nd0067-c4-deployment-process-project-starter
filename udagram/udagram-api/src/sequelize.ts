120c0bd597111157f2892d6fbaa387d2caabeb59
wanje125 <whdhkswp07@naver.com>
2022-08-23 23:36:22 +0900

    'f'

--- udagram/udagram-api/src/sequelize.ts
+++ udagram/udagram-api/src/sequelize.ts
@@ -5,8 +5,8 @@ export const sequelize = new Sequelize({
   username: config.username,
   password: config.password,
   database: config.database,
-  host: config.host,
-
+    host: config.host,
+    validateOnly:true,
   dialect: "postgres",
   storage: ":memory:",
 });
